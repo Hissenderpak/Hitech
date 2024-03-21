421298904d0deb9ce21f2494a451a0f9bb500cf3
Hissenderpak <mantuunbuuz2007@gmail.com>
2024-03-21 20:41:05 +0800

    asdasd

---
+++ pages/mockdata/mainMockData.js
@@ -0,0 +1,34 @@
+const mainMockData = [
+    {
+        id: 1,
+        name: "ASUS VZ24EHF Eye Care Gaming Monitor – 24-inch (23.8-inch viewable), IPS, Full HD, Frameless, 100Hz, Adaptive-Sync, 1ms MPRT, HDMI    ",
+        price: 350000,
+        information: "24-inch Full HD (1920 x 1080) IPS gaming monitor with stylish ultra-slim profile only 6.5mm thin SmoothMotion 100Hz refresh rate and 1ms MPRT delivers a seamless, tear-free visual experience by enabling VRR (variable refresh rate) VESA wall-mountable to save on desktop space TUV Rheinland Certification for Flicker-free and Low Blue Light technology to ensure a comfortable viewing experience",
+        inStock: 5,
+        performance: {
+            screenSize: 24,
+            refreshRate:"100Ghz",
+            brand: "Asus",
+            maxResolution: "1920x1080",
+            ports: "HDMI(v1.4) x 1"
+        },
+        mainImg: "https://www.hitech.mn/_next/image?url=https%3A%2F%2Fhitech-bc-main.s3.ap-southeast-1.amazonaws.com%2Fc57a5160-9884-4837-be1c-e63f6dd1e3e9.jpg&w=1920&q=75",
+        additionalImg: "https://www.hitech.mn/_next/image?url=https%3A%2F%2Fhitech-bc-description.s3.ap-southeast-1.amazonaws.com%2Fcf9ee4574ea595d37b2def4917d382fc.jpg&w=1920&q=75",
+    },
+    {
+        id: 2,
+        name: "ASUS VZ24EHF Eye Care Gaming Monitor – 24-inch (23.8-inch viewable), IPS, Full HD, Frameless, 100Hz, Adaptive-Sync, 1ms MPRT, HDMI    ",
+        price: 350000,
+        information: "24-inch Full HD (1920 x 1080) IPS gaming monitor with stylish ultra-slim profile only 6.5mm thin SmoothMotion 100Hz refresh rate and 1ms MPRT delivers a seamless, tear-free visual experience by enabling VRR (variable refresh rate) VESA wall-mountable to save on desktop space TUV Rheinland Certification for Flicker-free and Low Blue Light technology to ensure a comfortable viewing experience",
+        inStock: 5,
+        performance: {
+            screenSize: 24,
+            refreshRate:"100Ghz",
+            brand: "Asus",
+            maxResolution: "1920x1080",
+            ports: "HDMI(v1.4) x 1"
+        },
+        mainImg: "https://www.hitech.mn/_next/image?url=https%3A%2F%2Fhitech-bc-main.s3.ap-southeast-1.amazonaws.com%2Fc57a5160-9884-4837-be1c-e63f6dd1e3e9.jpg&w=1920&q=75",
+        additionalImg: "https://www.hitech.mn/_next/image?url=https%3A%2F%2Fhitech-bc-description.s3.ap-southeast-1.amazonaws.com%2Fcf9ee4574ea595d37b2def4917d382fc.jpg&w=1920&q=75",
+    },
+]

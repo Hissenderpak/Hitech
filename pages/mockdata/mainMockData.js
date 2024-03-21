d374cc7d03fe47b8cc97a9ef6b9a523ef3ed630e
Hissenderpak <mantuunbuuz2007@gmail.com>
2024-03-21 22:27:23 +0800

    Anar

--- pages/mockdata/mainMockData.js
+++ pages/mockdata/mainMockData.js
@@ -1,34 +1,297 @@
-const mainMockData = [
+const computerMonitor = [
     {
         id: 1,
         name: "ASUS VZ24EHF Eye Care Gaming Monitor – 24-inch (23.8-inch viewable), IPS, Full HD, Frameless, 100Hz, Adaptive-Sync, 1ms MPRT, HDMI    ",
         price: 350000,
         information: "24-inch Full HD (1920 x 1080) IPS gaming monitor with stylish ultra-slim profile only 6.5mm thin SmoothMotion 100Hz refresh rate and 1ms MPRT delivers a seamless, tear-free visual experience by enabling VRR (variable refresh rate) VESA wall-mountable to save on desktop space TUV Rheinland Certification for Flicker-free and Low Blue Light technology to ensure a comfortable viewing experience",
         inStock: 5,
-        performance: {
-            screenSize: 24,
-            refreshRate:"100Ghz",
-            brand: "Asus",
-            maxResolution: "1920x1080",
-            ports: "HDMI(v1.4) x 1"
-        },
+        performance: [
+            {
+                type: "Refresh Rate(Max)",
+                value: "100GHZ",
+            },
+            {
+                type: "Response Time",
+                value: "1ms MPRT",
+            },
+            {
+                type: "Panel Type",
+                value: "IPS",
+            },
+            {
+                type: "Backlight",
+                value: "LED",
+            },
+            {
+                type: "Panel Size(inch)",
+                value: "27",
+            },
+            {
+                type: "Resolution",
+                value: "1920x1080",
+            }
+        ],
         mainImg: "https://www.hitech.mn/_next/image?url=https%3A%2F%2Fhitech-bc-main.s3.ap-southeast-1.amazonaws.com%2Fc57a5160-9884-4837-be1c-e63f6dd1e3e9.jpg&w=1920&q=75",
         additionalImg: "https://www.hitech.mn/_next/image?url=https%3A%2F%2Fhitech-bc-description.s3.ap-southeast-1.amazonaws.com%2Fcf9ee4574ea595d37b2def4917d382fc.jpg&w=1920&q=75",
     },
+    
     {
         id: 2,
-        name: "ASUS VZ24EHF Eye Care Gaming Monitor – 24-inch (23.8-inch viewable), IPS, Full HD, Frameless, 100Hz, Adaptive-Sync, 1ms MPRT, HDMI    ",
-        price: 350000,
-        information: "24-inch Full HD (1920 x 1080) IPS gaming monitor with stylish ultra-slim profile only 6.5mm thin SmoothMotion 100Hz refresh rate and 1ms MPRT delivers a seamless, tear-free visual experience by enabling VRR (variable refresh rate) VESA wall-mountable to save on desktop space TUV Rheinland Certification for Flicker-free and Low Blue Light technology to ensure a comfortable viewing experience",
+        name: "ASUS - VZ27EHF Eye Care – 27 inch Full HD (1920 x 1080), IPS 100Hz",
+        price: 460000,
+        information: "ASUS VZ27EHF Eye Care Gaming Monitor – 27 inch Full HD (1920 x 1080), IPS, Frameless, 100Hz, Adaptive-Sync, 1ms MPRT, HDMI, Low Blue Light, Flicker Free, Wall Mountable 27-inch Full HD (1920 x 1080) IPS gaming monitor with stylish ultra-slim profile only 6.5mm thin SmoothMotion 100Hz refresh rate and 1ms MPRT delivers a seamless, tear-free visual experience by enabling VRR (variable refresh rate) VESA wall-mountable to save on desktop space TUV Rheinland Certification for Flicker-free and Low Blue Light technology to ensure a comfortable viewing experience",
         inStock: 5,
-        performance: {
-            screenSize: 24,
-            refreshRate:"100Ghz",
-            brand: "Asus",
-            maxResolution: "1920x1080",
-            ports: "HDMI(v1.4) x 1"
-        },
-        mainImg: "https://www.hitech.mn/_next/image?url=https%3A%2F%2Fhitech-bc-main.s3.ap-southeast-1.amazonaws.com%2Fc57a5160-9884-4837-be1c-e63f6dd1e3e9.jpg&w=1920&q=75",
-        additionalImg: "https://www.hitech.mn/_next/image?url=https%3A%2F%2Fhitech-bc-description.s3.ap-southeast-1.amazonaws.com%2Fcf9ee4574ea595d37b2def4917d382fc.jpg&w=1920&q=75",
+        performance: [
+            {
+                type: "Refresh Rate(Max)",
+                value: "100GHZ",
+            },
+            {
+                type: "Response Time",
+                value: "1ms MPRT",
+            },
+            {
+                type: "Panel Type",
+                value: "IPS",
+            },
+            {
+                type: "Backlight",
+                value: "LED",
+            },
+            {
+                type: "Panel Size(inch)",
+                value: "27",
+            },
+            {
+                type: "Resolution",
+                value: "1920x1080",
+            }
+        ],
+        mainImg: "https://www.hitech.mn/_next/image?url=https%3A%2F%2Fhitech-bc-main.s3.ap-southeast-1.amazonaws.com%2Fcbb595fe-42cf-431d-9902-d96cc3880f06.jpg&w=1920&q=75",
+        additionalImg: "https://www.hitech.mn/_next/image?url=https%3A%2F%2Fhitech-bc-main.s3.ap-southeast-1.amazonaws.com%2F739506a6-5428-4441-9aab-e5c19674acd3.jpg&w=1080&q=75",
+    },
+    
+    {
+        id: 3,
+        name: "ASUS TUF Gaming VG249QM1A 23.8 270 Hz Gaming Monitor",
+        price: 750000,
+        information: "23.8-inch Full HD (1920 x 1080) Fast IPS gaming monitor with ultrafast 270Hz (OC) refresh rate designed for professional gamers and immersive gameplay ASUS Extreme Low Motion Blur (ELMB) technology enables a 1ms response time (GTG), eliminating ghosting for sharp gaming visuals FreeSync Premium and G-Sync compatible delivers a seamless, tear-free gaming experience by enabling VRR (variable refresh rate) by default Supports both Adaptive-Sync with NVIDIA GeForce* graphics cards and Freesync Premium with AMD graphics cards. *Compatible with NVIDIA GeForce GTX 10 series, GTX 16 series, RTX 20 series and newer graphics cards What’s in the box: Power Cord, DisplayPort Cable, Warranty Card, Quick Start Guide",
+        inStock: 5,
+        performance: [
+            {
+                type: "Features",
+                value: "G-SYNC Compatible",
+            },
+            {
+                type: "Speaker",
+                value: "Yes, 2x 2 W",
+            },
+            {
+                type: "Max Resolution",
+                value: "1920 x 1080 Full HD",
+            },
+            {
+                type: "Panel Type",
+                value: "IPS-Type LCD",
+            },
+            {
+                type: "Refresh Rate (Max)",
+                value: "270GHz",
+            },
+            {
+                type: "Panel Size (inch)",
+                value: "23.8",
+            },
+            {
+                type: "Response Time",
+                value: "1ms",
+            }
+        ],
+        mainImg: "https://www.hitech.mn/_next/image?url=https%3A%2F%2Fhitech-bc-main.s3.ap-southeast-1.amazonaws.com%2Fad0393eb-68fd-4390-b357-aec618b6cdf0.jpg&w=1920&q=75",
+        additionalImg: "https://www.hitech.mn/_next/image?url=https%3A%2F%2Fhitech-bc-main.s3.ap-southeast-1.amazonaws.com%2F9787790a-306b-4b50-83b1-12da67a591b9.jpg&w=1920&q=75",
+    },
+
+    {
+        id: 4,
+        name: "ASUS TUF Gaming VG27AQL3A-W 27 inch QHD(2560x1440) 180Hz 27 - White",
+        price: 950000,
+        information: "27-inch QHD(2560x1440) gaming monitor with 180Hz refresh rate designed for professional gamers and immersive gameplay ASUS Extreme Low Motion Blur Sync (ELMB Sync) technology enables ELMB and variable-refresh-rate technologies to work simultaneously to eliminate ghosting and tearing for sharp gaming visuals with high frame rates FreeSync Premium and G-Sync compatible delivers a seamless, tear-free gaming experience by enabling VRR (variable refresh rate) by default High Dynamic Range (HDR) technology with DisplayHDR™ 400 compliance supports HDR-10 format to enhance bright and dark areas",
+        inStock: 3,
+        performance: [
+            {
+                type: "Response Time",
+                value: "1ms (GTG)",
+            },
+            {
+                type: "Refresh Rate (Max)",
+                value: "180Hz",
+            },
+            {
+                type: "Features",
+                value: "G-Sync compatible",
+            },
+            {
+                type: "Max Resolution",
+                value: "2560x1440",
+            },
+            {
+                type: "Panel Size (inch)",
+                value: "27",
+            },
+        ],
+        mainImg: "https://www.hitech.mn/_next/image?url=https%3A%2F%2Fhitech-bc-main.s3.ap-southeast-1.amazonaws.com%2F6d36d4c8-f9a6-4f11-8b7f-d24030a1ad67.jpg&w=1920&q=75",
+        additionalImg: "https://www.hitech.mn/_next/image?url=https%3A%2F%2Fhitech-bc-main.s3.ap-southeast-1.amazonaws.com%2F161f3de2-4207-490d-9785-206f840792a7.jpg&w=1920&q=75",
+    },
+
+    {
+        id: 5,
+        name: "ASUS ProArt Display PA279CV Professional Monitor",
+        price: 1300000,
+        information: "27-inch 4K UHD (3840 x 2160) LED backlight display with IPS 178° wide viewing angle panel International color standard 100% sRGB and 100% Rec. 709 wide color gamut Calman Verified with factory calibrated for excellent Delta E < 2 color accuracy Extensive connectivity including DP over USB-C with 65W Power Delivery, DisplayPort, HDMI, USB hub Adaptive-Sync technology (40~60Hz) to animated content with fast action and eliminate screen tearing ASUS-exclusive ProArt Preset and ProArt Palette provide numerous adjustable color parameters Ergonomic design with +35° ~ -5° tilt, ±45° swivel, ±90° pivot and 150mm height adjustments for a comfortable viewing experience",
+        inStock: 5,
+        performance: [
+            {
+                type: "Backlight Type",
+                value: "LED",
+            },
+            {
+                type: "Panel Type",
+                value: "IPS",
+            },
+            {
+                type: "Refresh Rate (Max)",
+                value: "60hz",
+            },
+            {
+                type: "Features",
+                value: "ASUS Flicker-Free technology",
+            },
+            {
+                type: "Response Time",
+                value: "5ms",
+            },
+            {
+                type: "Max Resolution",
+                value: "3840x2160",
+            },
+            {
+                type: "Color Space (sRGB)",
+                value: "100%",
+            },
+        ],
+        mainImg: "https://www.hitech.mn/_next/image?url=https%3A%2F%2Fhitech-bc-main.s3.ap-southeast-1.amazonaws.com%2F31d8936c-4009-4171-b84d-aa3a73024df0.jpg&w=1920&q=75",
+        additionalImg: "https://www.hitech.mn/_next/image?url=https%3A%2F%2Fhitech-bc-main.s3.ap-southeast-1.amazonaws.com%2F8c4dace3-eb41-4d14-839f-ed2837c46034.jpg&w=1920&q=75",
+    },
+
+    {
+        id: 6,
+        name: "Dell UltraSharp U2722DX 27 LCD Monitor",
+        price: 1350000,
+        information: "See images with 1.77 times more details than Full HD with 2560x1440 resolution on this 27-inch QHD display. Seamless view: Expand your view with virtually borderless InfinityEdge. With a four-sided, ultrathin bezel, you can work with a clear, edge-to-edge display. Use the daisy-chain feature for a seamless multi-monitor setup. Boost productivity: Increase your productivity by up to 21% when you use two monitors together. The OSD (Onscreen Menu Display) settings from one monitor can be synchronized across connected monitors for easy multi-monitor setup. Extensive connectivity: Easily connect to your devices with comprehensive connectivity, including DP1.4, HDMI 2.0. Quick access ports at the front of the monitor allow convenient connection to peripherals and mobile phone charging with up to 15W via USB-C. Multitask effectively: Connect two PCs to the monitor and view content from both PC sources at the same time with PBP (Picture-by-Picture) and PIP (Picture-in-Picture) features.",
+        inStock: 3,
+        performance: [
+            {
+                type: "Screen Size",
+                value: "27",
+            },
+            {
+                type: "Brand",
+                value: "DELL",
+            },
+            {
+                type: "Max Resolution",
+                value: "2560x1440",
+            },
+            {
+                type: "Refresh Rate (Max)",
+                value: "60",
+            },
+            {
+                type: "Backlight Type",
+                value: "LCD",
+            },
+            {
+                type: "Response Time",
+                value: "8ms",
+            },
+        ],
+        mainImg: "https://www.hitech.mn/_next/image?url=https%3A%2F%2Fhitech-bc-main.s3.ap-southeast-1.amazonaws.com%2F57d5539a-daa0-49b2-bfc0-9b3a1d91be50.jpg&w=1920&q=75",
+        additionalImg: "https://www.hitech.mn/_next/image?url=https%3A%2F%2Fhitech-bc-main.s3.ap-southeast-1.amazonaws.com%2F0b74afcd-695d-4a59-9740-5dbeecef71ac.jpg&w=1920&q=75",
+    },
+
+    {
+        id: 7,
+        name: "BenQ Zowie XL2566K 24.5 Fast TN in 360Hz Gaming Monitor",
+        price: 2350000,
+        information: "24.5'' Fast TN in 360Hz Gaming Monitor | Motion Clarity DyAc⁺ | 1080p | XL Setting to Share | Custom Quick Menu | S Switch | Shield | Smaller Base | Adjustable Height & Tilt, Black",
+        inStock: 5,
+        performance: [
+            {
+                type: "Panel Size (inch)",
+                value: "24.5",
+            },
+            {
+                type: "Max Resolution",
+                value: "1920 x 1080 (1080p)",
+            },
+            {
+                type: "Backlight Type",
+                value: "LCD",
+            },
+            {
+                type: "Features",
+                value: "DyAc⁺™ Technology",
+            },
+            {
+                type: "Panel Type",
+                value: "TN",
+            },
+            {
+                type: "Response Time",
+                value: "1.8ms",
+            },
+            {
+                type: "Refresh Rate(Max)",
+                value: "360GHz",
+            },
+        ],
+        mainImg: "https://www.hitech.mn/_next/image?url=https%3A%2F%2Fhitech-bc-main.s3.ap-southeast-1.amazonaws.com%2F57d5539a-daa0-49b2-bfc0-9b3a1d91be50.jpg&w=1920&q=75",
+        additionalImg: "https://www.hitech.mn/_next/image?url=https%3A%2F%2Fhitech-bc-description.s3.ap-southeast-1.amazonaws.com%2F40cc147573f03ec8ee9f0db3fdfb8f09.jpg&w=1920&q=75s",
+    },
+
+    {
+        id: 8,
+        name: "ASUS ROG Strix 27” 1440P EVA Edition HDR Gaming Monitor",
+        price: 2500000,
+        information: "27-inch QHD (2560 x 1440) EVA Edition HDR gaming monitor with ultrafast 270Hz (overclocking) refresh rate designed for professional gamers and immersive gameplay.Viewing Angle:170.0 degrees.Specific uses for product - Gaming ASUS Fast IPS technology enables a 0.5ms response time (GTG) for sharp gaming visuals with high frame rates ASUS Extreme Low Motion Blur Sync (ELMB SYNC) technology enables ELMB together with G-SYNC Compatible, eliminating ghosting and tearing for sharp gaming visuals with high frame rates High Dynamic Range (HDR) technology with professional color gamut delivers contrast and color performance that meets the DisplayHDR 400 certification G-SYNC Compatible, delivering a seamless, tear-free gaming experience Full HD(1920 X 1080)@120Hz & 1440P (2560 x 1440)@60Hz output on PS5, 1440P(2560 x 1440)@120Hz VRR output on Xbox Series X/S Robust connectivity with DisplayPort 1.4 DSC x2, HDMI (v2.0) x2, and USB 3.0 x2 ports What’s in the box: HDMI cable, DisplayPort cable, USB cable, Power cord & adapter, Calibration report, ROG stickers, Warranty card, Quick Start Guide",
+        inStock: 1,
+        performance: [
+            {
+                type: "Features",
+                value: "FreeSync™ Premium",
+            },
+            {
+                type: "Max Resolution",
+                value: "2560x1440",
+            },
+            {
+                type: "Refresh Rate (Max)",
+                value: "270Hz",
+            },
+            {
+                type: "Response Time",
+                value: "0.5ms",
+            },
+            {
+                type: "Panel Type",
+                value: "IPS",
+            },
+            {
+                type: "Panel Size (inch)",
+                value: "27inch",
+            },
+        ],
+        mainImg: "https://www.hitech.mn/_next/image?url=https%3A%2F%2Fhitech-bc-main.s3.ap-southeast-1.amazonaws.com%2F57d5539a-daa0-49b2-bfc0-9b3a1d91be50.jpg&w=1920&q=75",
+        additionalImg: "https://www.hitech.mn/_next/image?url=https%3A%2F%2Fhitech-bc-main.s3.ap-southeast-1.amazonaws.com%2F0b74afcd-695d-4a59-9740-5dbeecef71ac.jpg&w=1920&q=75",
     },
 ]

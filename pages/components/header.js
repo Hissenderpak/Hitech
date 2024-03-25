4fb77508ef7ba8191ce5ae12b56d93622f1f9ac4
Hissenderpak <mantuunbuuz2007@gmail.com>
2024-03-26 02:19:59 +0800

    asd

--- pages/components/header.js
+++ pages/components/header.js
@@ -8,13 +8,13 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Header() {
   const router = useRouter();
-  const [dropDown, setDropDown] = useState(false);
+  const [dropDown, setDropDown] = useState(true);
 
   return (
     <div className="w-full fixed shadow flex flex-col relative items-center">
       <div className="w-full h-20 px-4 flex items-center justify-between  md:px-12 xl:px-28 ">
 
-        <div  className="w-fit flex items-center xl:flex-row-reverse">
+        <div  className="w-fit flex items-center flex-row-reverse">
           <img onClick={() => router.push('./index.js')} src="https://www.hitech.mn/assets/icons/ic_logo.svg" alt="" className="w-[108px]" />
           <button onClick={() => setDropDown(!dropDown)} className="h-full w-[48px] px-4 mr-[12px]">
             {
@@ -59,15 +59,84 @@ export default function Header() {
       <div className="h-[1px] w-full bg-gray-200"></div>
 
       {!dropDown && (
-        <div className="absolute p-[20px] bg-gray-200 rounded-lg mt-[85px] grid grid-cols-4">
-          <div className="px-2 py-1 flex items-center hover:bg-white rounded xl:px-2 xl:py-4 relative">
-            <img className="w-6 mr-[6px]" src="https://hitech-bc-categories.s3.ap-southeast-1.amazonaws.com/6367cd4f988c93bf0e8bcbf30618e715.svg"/>
-            <span className="text-sm text-black-500 hover:text-orange-600 mr-4 text-clip">Суурин Компьютерын эд анги</span>
-            <img className=" flex h-[8px] flex justify-center items-center" src="https://www.hitech.mn/assets/icons/ic_chevron_right_black.svg"/>
+        <div className="hidden md:grid absolute p-[20px] bg-gray-100 rounded-lg mt-[87px]  grid-cols-4 mx-4 lg:mx-20 2xl:mx- z-20 ">
+          <div className="px-2 py-1 rounded flex h-full items-center hover:bg-white min-h-[56px] hover:text-orange-600 text-clip cursor-pointer">
+            <img className="mr-3 w-6 h-6" src="https://hitech-bc-categories.s3.ap-southeast-1.amazonaws.com/6367cd4f988c93bf0e8bcbf30618e715.svg"/>
+            <span className="mr-3 text-black-500 text-sm flex-1">Суурин компьютер эд анги</span>
+            <img className="w-[4px] mx-[10px]" src="https://www.hitech.mn/assets/icons/ic_chevron_right_black.svg"/>
+          </div>
+          <div className="px-2 py-1 rounded flex h-full items-center hover:bg-white min-h-[56px] hover:text-orange-600 text-clip cursor-pointer">
+            <img className="mr-3 w-6 h-6" src="https://hitech-bc-categories.s3.ap-southeast-1.amazonaws.com/c6ee3cd271bb3eeb560fb7839da7210b.svg"/>
+            <span className="mr-3 text-black-500 text-sm flex-1">Компьютерын дагалдах хэрэгсэл</span>
+            <img className="w-[4px] mx-[10px]" src="https://www.hitech.mn/assets/icons/ic_chevron_right_black.svg"/>
+          </div>
+          <div className="px-2 py-1 rounded flex h-full items-center hover:bg-white min-h-[56px] hover:text-orange-600 text-clip cursor-pointer">
+            <img className="mr-3 w-6 h-6" src="https://hitech-bc-categories.s3.ap-southeast-1.amazonaws.com/e0a6f85e189a11fc27b009c360a61713.svg"/>
+            <span className="mr-3 text-black-500 text-sm flex-1">Ширээ / Сандал / Гэрэл</span>
+            <img className="w-[4px] mx-[10px]" src="https://www.hitech.mn/assets/icons/ic_chevron_right_black.svg"/>
+          </div>
+          <div className="px-2 py-1 rounded flex h-full items-center hover:bg-white min-h-[56px] hover:text-orange-600 text-clip cursor-pointer">
+            <img className="mr-3 w-6 h-6" src="https://hitech-bc-categories.s3.ap-southeast-1.amazonaws.com/4b3189bd171a9640e6e433a87f4c2f84.svg"/>
+            <span className="mr-3 text-black-500 text-sm flex-1">Компьютерын дэлгэц</span>
+            <img className="w-[4px] mx-[10px]" src="https://www.hitech.mn/assets/icons/ic_chevron_right_black.svg"/>
+          </div>
+          <div className="px-2 py-1 rounded flex h-full items-center hover:bg-white min-h-[56px] hover:text-orange-600 text-clip cursor-pointer">
+            <img className="mr-3 w-6 h-6" src="https://hitech-bc-categories.s3.ap-southeast-1.amazonaws.com/d719eee66d476b7791dc738bc35a157d.svg"/>
+            <span className="mr-3 text-black-500 text-sm flex-1">Бэлэн компьютер</span>
+            <img className="w-[4px] mx-[10px]" src="https://www.hitech.mn/assets/icons/ic_chevron_right_black.svg"/>
+          </div>
+          <div className="px-2 py-1 rounded flex h-full items-center hover:bg-white min-h-[56px] hover:text-orange-600 text-clip cursor-pointer">
+            <img className="mr-3 w-6 h-6" src="https://hitech-bc-categories.s3.ap-southeast-1.amazonaws.com/db974f85efd6e4ed5feb5965aa884651.svg"/>
+            <span className="mr-3 text-black-500 text-sm flex-1">Зөөврийн компьютер / Дагалдах хэрэгсэл</span>
+            <img className="w-[4px] mx-[10px]" src="https://www.hitech.mn/assets/icons/ic_chevron_right_black.svg"/>
+          </div>
+          <div className="px-2 py-1 rounded flex h-full items-center hover:bg-white min-h-[56px] hover:text-orange-600 text-clip cursor-pointer">
+            <img className="mr-3 w-6 h-6" src="https://www.hitech.mn/_next/image?url=https%3A%2F%2Fhitech-bc-categories.s3.ap-southeast-1.amazonaws.com%2FSmartphone-1618934806605841691-original.png&w=32&q=75"/>
+            <span className="mr-3 text-black-500 text-sm flex-1">Гар утас, Таблет / Дагалдах хэрэгсэл</span>
+            <img className="w-[4px] mx-[10px]" src="https://www.hitech.mn/assets/icons/ic_chevron_right_black.svg"/>
+          </div>
+          <div className="px-2 py-1 rounded flex h-full items-center hover:bg-white min-h-[56px] hover:text-orange-600 text-clip cursor-pointer">
+            <img className="mr-3 w-6 h-6" src="https://www.hitech.mn/_next/image?url=https%3A%2F%2Fhitech-bc-categories.s3.ap-southeast-1.amazonaws.com%2FConsole-1622554218475530105-original.png&w=32&q=75"/>
+            <span className="mr-3 text-black-500 text-sm flex-1">Консоль</span>
+            <img className="w-[4px] mx-[10px]" src="https://www.hitech.mn/assets/icons/ic_chevron_right_black.svg"/>
+          </div>
+          <div className="px-2 py-1 rounded flex h-full items-center hover:bg-white min-h-[56px] hover:text-orange-600 text-clip cursor-pointer">
+            <img className="mr-3 w-6 h-6" src="https://hitech-bc-categories.s3.ap-southeast-1.amazonaws.com/dc95a943f35d833f4785aea8648620b4.svg"/>
+            <span className="mr-3 text-black-500 text-sm flex-1">Сүлжээний төхөөрөмж</span>
+            <img className="w-[4px] mx-[10px]" src="https://www.hitech.mn/assets/icons/ic_chevron_right_black.svg"/>
+          </div>
+          <div className="px-2 py-1 rounded flex h-full items-center hover:bg-white min-h-[56px] hover:text-orange-600 text-clip cursor-pointer">
+            <img className="mr-3 w-6 h-6" src="https://hitech-bc-categories.s3.ap-southeast-1.amazonaws.com/6367cd4f988c93bf0e8bcbf30618e715.svg"/>
+            <span className="mr-3 text-black-500 text-sm flex-1">Принтер / Сканнер</span>
+            <img className="w-[4px] mx-[10px]" src="https://www.hitech.mn/assets/icons/ic_chevron_right_black.svg"/>
+          </div>
+          <div className="px-2 py-1 rounded flex h-full items-center hover:bg-white min-h-[56px] hover:text-orange-600 text-clip cursor-pointer">
+            <img className="mr-3 w-6 h-6" src="https://www.hitech.mn/_next/image?url=https%3A%2F%2Fhitech-bc-categories.s3.ap-southeast-1.amazonaws.com%2Fcamera-1627164869115536692-original.png&w=32&q=75"/>
+            <span className="mr-3 text-black-500 text-sm flex-1">Камер / Дуран</span>
+            <img className="w-[4px] mx-[10px]" src="https://www.hitech.mn/assets/icons/ic_chevron_right_black.svg"/>
+          </div>
+          <div className="px-2 py-1 rounded flex h-full items-center hover:bg-white min-h-[56px] hover:text-orange-600 text-clip cursor-pointer">
+            <img className="mr-3 w-6 h-6" src="https://hitech-bc-categories.s3.ap-southeast-1.amazonaws.com/2822b50bc5c6fa6de43e70f35865db6e.svg"/>
+            <span className="mr-3 text-black-500 text-sm flex-1">Үйлдлийн систем / Лиценз</span>
+            <img className="w-[4px] mx-[10px]" src="https://www.hitech.mn/assets/icons/ic_chevron_right_black.svg"/>
+          </div>
+          <div className="px-2 py-1 rounded flex h-full items-center hover:bg-white min-h-[56px] hover:text-orange-600 text-clip cursor-pointer">
+            <img className="mr-3 w-6 h-6" src="https://www.hitech.mn/_next/image?url=https%3A%2F%2Fhitech-bc-categories.s3.ap-southeast-1.amazonaws.com%2Ffigure-1627491458269595657-original.png&w=32&q=75"/>
+            <span className="mr-3 text-black-500 text-sm flex-1">Фигур / Сувинер</span>
+            <img className="w-[4px] mx-[10px]" src="https://www.hitech.mn/assets/icons/ic_chevron_right_black.svg"/>
+          </div>
+          <div className="px-2 py-1 rounded flex h-full items-center hover:bg-white min-h-[56px] hover:text-orange-600 text-clip cursor-pointer">
+            <img className="mr-3 w-6 h-6" src="https://hitech-bc-categories.s3.ap-southeast-1.amazonaws.com/3b8d9d2d4c1c4d9ccd185efccc1f8aab.svg"/>
+            <span className="mr-3 text-black-500 text-sm flex-1">Фигур / Сувинер</span>
+            <img className="w-[4px] mx-[10px]" src="https://www.hitech.mn/assets/icons/ic_chevron_right_black.svg"/>
+          </div>
+          <div className="px-2 py-1 rounded flex h-full items-center hover:bg-white min-h-[56px] hover:text-orange-600 text-clip cursor-pointer ">
+            <img className="mr-3 w-6 h-6" src="https://www.hitech.mn/_next/image?url=https%3A%2F%2Fhitech-bc-categories.s3.ap-southeast-1.amazonaws.com%2F152314932_890965088371357_1292868159020091712_n-1617035274012827452-full.png&w=32&q=75"/>
+            <span className="mr-3 text-black-500 text-sm flex-1">Бусад</span>
+            <img className="w-[4px] mx-[10px]" src="https://www.hitech.mn/assets/icons/ic_chevron_right_black.svg"/>
           </div>
         </div>
       )}
-
 
       <div className="hidden md:flex justify-center h-[32px]">
         <div className="h-full w-fit px-4 text-sm flex items-center text-black-500">Бэлэн Компьютер</div>
